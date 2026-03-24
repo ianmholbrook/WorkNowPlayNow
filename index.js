c657b220c558b6188f5802948873aba881880f90
Robonics014 <ezj213@gmail.com>
2026-03-24 12:07:26 -0400

    App modified to support EJS as views engine

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
+const ejs = require('ejs');
 
-app.use(cors);
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.get("/", (req, res) => {
-	res.send("Hello Dev!");
+	res.render("index.ejs", {});
 });
 
-app.listen(8080, () => {
+app.listen( 8080, () => {
 	console.log("App launched! Go to http://localhost:8080/ to see");
 });
